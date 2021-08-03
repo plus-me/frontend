1b06878cd7b2ff84699e148039dafbc0de5f7f7d
Niklas Computer\Niklas <niklasschemmer@googlemail.com>
2021-08-03 14:22:59 +0200

    Finish onboarding

--- src/pages/onboarding/onboarding.component.ts
+++ src/pages/onboarding/onboarding.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { UserActions } from 'src/libs/actions/users.actions';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   templateUrl: './onboarding.component.html',
+  styleUrls: ['onboarding.component.scss']
 })
 export class OnboardingComponent {
+  @ViewChild(IonSlides) slides: IonSlides;
+  activeSlide = 0;
   public constructor(
     private store: Store,
   ) {}
 
   public finish() {
     this.store.dispatch(new UserActions.FinishedOnboarding());
+  }
+
+  public skipOnboarding() {
+    if(this.activeSlide !== 4)
+      this.store.dispatch(new UserActions.FinishedOnboarding());
+  }
+
+  public slideChanged() {
+    this.slides.lockSwipeToPrev(true);
+    this.slides.getActiveIndex().then(index => {
+      this.activeSlide = index;
+    });
   }
 }
