77a416505babb6f85d4dc7e1acf2c0407effba56
ri0t <riot@c-base.org>
2021-08-23 21:16:07 +0200

    complete-profile-actions

--- src/libs/actions/users.actions.ts
+++ src/libs/actions/users.actions.ts
@@ -12,12 +12,33 @@ export namespace UserActions {
     public static readonly type = '[Users] Logout';
   }
 
+  export class DeleteAction {
+    public static readonly type = '[Users] Delete user profile';
+  }
+
   export class RegisterAction {
     public static readonly type = '[Users] Register';
 
     public constructor(
       public email: string,
       public password: string,
+    ) {}
+  }
+
+  export class UpdatePassword {
+    public static readonly type = '[Users] Set a new password';
+
+    public constructor(
+      public password: string,
+      public new_password: string,
+    ) {}
+  }
+
+  export class ResetPassword {
+    public static readonly type = '[Users] Request a password reset link';
+
+    public constructor(
+      public email: string,
     ) {}
   }
 
